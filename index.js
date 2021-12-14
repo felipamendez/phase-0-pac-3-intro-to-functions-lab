ebec2b6fdf83d06d9c09e9f58c2b314da0c15d08
Felipa Mendez <felipalouise@gmail.com>
2021-12-14 15:08:30 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // your code here
+function shout (string) {
+    return string.toUpperCase();
+}
+
+function whisper (string) {
+    return string.toLowerCase();
+}
+
+function logShout (string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper (string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoomate (string) {
+    if (string === string.toLowerCase()) {
+        return ("I can\'t hear you!")}; 
+        if (string === string.toUpperCase()) {
+            return "YES INDEED!"};
+            if (string = "I love you, Grandma.") {
+                return "I would love to!"};
+            }
+
+           
+    
+    
+
+    
